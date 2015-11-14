bb6161ec0cfa30120263585d8129f263c27aa16b
vatsala <vatsala.swaroop@gmail.com>
2015-11-15 01:00:16 +0530

    Adding tabs

--- levels/level_2/apps.js
+++ levels/level_2/apps.js
@@ -5,6 +5,18 @@
     this.products = gems;
   });
 
+  app.controller('TabController', function(){
+    this.tab = 1;
+
+    this.setTab = function(newValue){
+      this.tab = newValue;
+    };
+
+    this.isSet = function(tabName){
+      return this.tab === tabName;
+    };
+  });
+
 
   app.controller('GalleryController', function(){
     this.current = 0;
