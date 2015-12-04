a1059048fd2fdb5e47578ae92dd2d85a1943cf73
vatsala <vatsala.swaroop@gmail.com>
2015-12-04 23:08:12 +0530

    Adding dependency

--- levels/level_5/app.js
+++ levels/level_5/app.js
@@ -1,5 +1,5 @@
 (function() {
-  var app = angular.module('gemStore', []);
+  var app = angular.module('gemStore', ['store-directives']);
 
   app.controller('StoreController', function(){
     this.products = gems;
