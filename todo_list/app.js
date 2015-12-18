cf38e6340272bd021a373bae5381267012a35fcf
vatsala <vatsala.swaroop@gmail.com>
2015-12-19 03:04:49 +0530

    Allowing to delete the item

--- todo_list/app.js
+++ todo_list/app.js
@@ -5,11 +5,20 @@ angular.module("todoListApp", [])
       $scope.todos = response.data;
     });
 
+  $scope.deleteTodo = function(todo, index) {
+    dataService.deleteTodo(todo);
+    $scope.todos.splice(index, 1);
+  };
+
 })
 .service('dataService', function($http) {
 
   this.getTodos = function(callback){
     $http.get('mock/todos.json').then(callback)
   };  
+
+  this.deleteTodo = function(todo) {
+    console.log("The " + todo.name + " todo has been deleted!");
+  };
   
 });
