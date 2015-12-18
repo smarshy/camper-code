f681c29f469f09d82fe72fdb9b2d1cababa5f258
vatsala <vatsala.swaroop@gmail.com>
2015-12-19 01:52:34 +0530

    To quit editing on clicking outside

--- todo_list/app.js
+++ todo_list/app.js
@@ -1,5 +1,12 @@
 angular.module("todoListApp", [])
 .controller('mainCtrl', function($scope) {
 
+$scope.todos = [
+    {"name": "clean the room"}, 
+    {"name": "write the mail"}, 
+    {"name": "visit the office"},
+    {"name": "verify the recipe"},
+    {"name": "Take bo to the vet"}
+  ]
 
 });
